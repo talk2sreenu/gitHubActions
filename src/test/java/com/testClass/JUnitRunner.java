05f9ed1031ed100ad87e8f63f55e640afbb79301
talk2sreenu <talk2sreenu@gmail.com>
2024-08-12 16:50:21 -0400

    gitHub Actions first commit

---
+++ src/test/java/com/testClass/JUnitRunner.java
@@ -0,0 +1,13 @@
+package com.testClass;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class JUnitRunner {
+	
+	@Test
+	void firstjUnitTest() {
+		assertEquals(true, true);
+	}	
+	
+}
